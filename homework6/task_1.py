2309a3dd23de83848357f68bfccc681196593114
Vadim Ungureanu <christoff.or.on@gmail.com>
2024-04-22 18:46:36 +0300

    add all homework6

--- homework6/task_1.py
+++ homework6/task_1.py
@@ -92,30 +92,41 @@ print(numbers)
 # Generează și printează o listă cu toate numerele pătrat perfect din intervalul [1, 100].
 
 # CODUL TĂU VINE MAI JOS:
-
+numere_patrat_perfect = [x ** 2 for x in range(1, 11)]
+print(numere_patrat_perfect)
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind un buclă for , printează tabla înmulțirii pentru numărul 7.
 
 # CODUL TĂU VINE MAI JOS:
-
+for i in range(1,11):
+    print(7 * i)
 # CODUL TĂU VINE MAI SUS:
 
 # Creează o listă de liste, unde fiecare sub-listă conține perechi (i, j) pentru i și j de la 1 la 5. Printează această listă de perechi.
 
 # CODUL TĂU VINE MAI JOS:
-
+perechi = [[i,j] for i in range(1,6) for j in range(1,6)]
+print(perechi)
 # CODUL TĂU VINE MAI SUS:
 
 # Parcurge lista de la punctul anterior și printează doar perechile unde i < j .
 
 # CODUL TĂU VINE MAI JOS:
-
+for i,j in perechi:
+    if i < j: print(i, j)
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind un buclă while , caută și printează prima valoare care este mai mare decât 10 dintr-o listă cu numere random creată de tine. Dacă nu există, printează "Nu există valori mai mari decât 10".
 
 # CODUL TĂU VINE MAI JOS:
+numere = (5, 6, 7, 4, 9, 8, 12, 22, 15, 16, 19, 30)
+i = 0
+while i < len(numere):
+    if numere[i] > 10:
+        print("Prima valoare mai mare decât 10 este:", numere[i],"pe pozitia:",i)
+        break
+    i += 1
 
 # CODUL TĂU VINE MAI SUS:
 
@@ -128,7 +139,14 @@ print(numbers)
 # *****
 
 # CODUL TĂU VINE MAI JOS:
-
+dimensiune = int(input("Introduceți dimensiunea pătratului: "))
+i = 0
+j = 0
+while i < dimensiune:
+    while j < dimensiune:
+        print("*" * dimensiune)
+        j += 1
+        i += 1
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind for sau while loops realizați afișările de mai jos
@@ -142,7 +160,13 @@ print(numbers)
 # 123456
 
 # CODUL TĂU VINE MAI JOS:
+dimensiune = int(input("Introduceți dimensiunea: "))
+for i in range(1, dimensiune + 1):
+    output = ""
+    for j in range(1, i + 1):
+        output += str(j)
 
+    print(output)
 # CODUL TĂU VINE MAI SUS:
 
 # Afișarea 2:
@@ -154,7 +178,13 @@ print(numbers)
 # 5
 
 # CODUL TĂU VINE MAI JOS:
-
+dimensiune = int(input("Introduceți dimensiunea: "))
+for i in range(dimensiune, 0 , -1):
+    output = ""
+    for j in range(i, 0, -1):
+        output += str(j)
+    
+    print(output)    
 # CODUL TĂU VINE MAI SUS:
 
 # Afișarea 3:
@@ -168,7 +198,10 @@ print(numbers)
 # g
 
 # CODUL TĂU VINE MAI JOS:
+s = "abcdefg"
 
+for i in range(len(s)):
+    print(s[i:])
 # CODUL TĂU VINE MAI SUS:
 
 # Afișarea 4:
@@ -183,7 +216,14 @@ print(numbers)
 # -+-+-+-+-+-+-+-+
 
 # CODUL TĂU VINE MAI JOS:
-
+dimensiune = int(input("Introduceți dimensiunea: "))
+for i in range(dimensiune + 1):
+    for j in range(dimensiune + 1):
+        if (i + j) % 2 == 0:
+            print("-", end="")
+        else:
+            print("+", end="")  
+    print()  
 # CODUL TĂU VINE MAI SUS:
 
 # Afișarea 5:
@@ -199,7 +239,15 @@ print(numbers)
 # 243
 
 # CODUL TĂU VINE MAI JOS:
-
+dimensiune = int(input("Introduceți dimensiunea: "))
+for i in range(dimensiune):
+    for j in range(i + 1):
+        print(3 ** (j + 1), end=" ")
+    print()
+for i in range(1, dimensiune):
+    for j in range(dimensiune - i):
+        print(3 ** (i + j + 1), end=" ")
+    print()
 # CODUL TĂU VINE MAI SUS:
 
 # Completați sarcinile de mai sus pentru a exersa lucrul cu buclele în Python.
